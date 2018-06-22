d163d2c5f7662528665447ad9ae6e6812fdb9020
juanrarodriguez18 <dragondr3w@gmail.com>
2018-06-22 23:35:35 +0200

    Terminadas utilidades de Scraping

--- utils/micholloScraping.py
+++ utils/micholloScraping.py
@@ -15,3 +15,49 @@
 #     You should have received a copy of the GNU General Public License
 #     along with ChollosAlert Bot.  If not, see <http:#www.gnu.org/licenses/>.
 
+# encoding:utf-8
+import codecs
+import sys
+import requests
+from repository.chollo import Chollo
+
+from bs4 import BeautifulSoup
+
+
+def extraer_datos_pagina():
+    url = 'https://michollo.com'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.text, "html.parser")
+    chollos = soup.find_all('div', {"class": "news-community"})
+
+    titulo_chollo = ''
+    comercio_chollo = ''
+    precio_chollo = ''
+    descripcion_chollo = ''
+    cupon_chollo = ''
+    link_chollo = ''
+    result = []
+
+    # Use "chcp 65001" command on windows console in order to show the string correctly
+    for chollo in chollos:
+        if chollo.find('h3') != None:
+            titulo_chollo = chollo.find('h3').text.encode('utf-8').decode('utf-8').strip()
+        if chollo.find('span', {"class": "single_price_count"}) != None:
+            precio_chollo = chollo.find('span', {"class": "single_price_count"}).text.encode('utf-8').decode('utf-8').strip().split()[0]
+        if chollo.find('div', {"class": "newscom_detail"}) != None:
+            descripcion_chollo = chollo.find('div', {"class": "newscom_detail"}).find('p').text.encode('utf-8').decode('utf-8').strip()
+        if chollo.find('span', {"class": "coupon_text"}) != None:
+            cupon_chollo = chollo.find('span', {"class": "coupon_text"}).text.encode('utf-8').decode('utf-8').strip()
+        if chollo.find('a', {"class": "btn_offer_block"}) != None:
+            link_chollo = chollo.find('a', {"class": "btn_offer_block"}).get('href').encode('utf-8').decode('utf-8').strip()
+        print(titulo_chollo+" - "+precio_chollo+" - "+descripcion_chollo+" - "+cupon_chollo+" - "+link_chollo)
+        cholloObject = Chollo(titulo_chollo, comercio_chollo, precio_chollo, descripcion_chollo, cupon_chollo, link_chollo)
+        result.append(cholloObject)
+        print(titulo_chollo+' - '+comercio_chollo+' - '+precio_chollo+' - '+descripcion_chollo+' - '+cupon_chollo+' - '+link_chollo)
+    
+    return result
+
+
+# extraer_datos_pagina()
