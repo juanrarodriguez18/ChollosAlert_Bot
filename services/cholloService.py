8e313a2c191796358b1772b487058753bcb66520
Juan Ramón Rodríguez <juanrarodriguez18@gmail.com>
2018-10-23 16:15:17 +0200

    Update cholloService.py

--- services/cholloService.py
+++ services/cholloService.py
@@ -88,6 +88,7 @@ def check_chollos_first_time(user_id):
         except Exception as e:
             logging.error("Failed checking chollos")
             logging.error(e)
+            logging.error("User ID: "+user_id)
             # get_bot().send_message(chat_id=user_id, parse_mode="Markdown", text="Something go really bad. You couldn't be notify of news chollos")
         return result
 
