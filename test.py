995fd39878434c0f4dbc0f9bc223b3eb242637e5
Juan Ramón Rodríguez <dragondr3w@gmail.com>
2020-10-29 12:36:15 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ from utils.chollometroScraping import extraer_datos_pagina_chollometro
 
 print("# MI CHOLLO")
 extraer_datos_pagina_michollo()
+
 print("# CHOLLOMETRO")
 extraer_datos_pagina_chollometro()
